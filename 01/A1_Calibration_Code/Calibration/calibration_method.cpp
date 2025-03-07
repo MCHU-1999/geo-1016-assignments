8d873a807738cabf9f8274f84aa5a512de9fe680
mchu-1999 <jerry.ee06@nctu.edu.tw>
2025-03-07 04:20:11 +0100

    new feature: verify intermediate params, display multiple params.

--- 01/A1_Calibration_Code/Calibration/calibration_method.cpp
+++ 01/A1_Calibration_Code/Calibration/calibration_method.cpp
@@ -28,7 +28,19 @@
 
 using namespace easy3d;
 
-
+bool equal_matrices(const Matrix &A, const Matrix &B) {
+    if (A.cols() == B.cols() && A.rows() == B.rows())
+    {
+        if ((A - B).resize(1, A.cols()*A.rows()).get_row(0).length() == 0)
+        {
+            return true;
+        } else {
+            return false;
+        }
+    } else {
+        return false;
+    }
+}
 
 double sin_from_cos(double cos_theta, bool positive = true) {
     double sin_theta = std::sqrt(1 - cos_theta * cos_theta);
@@ -64,19 +76,21 @@ std::vector<Vector2D> project_3D_to_2D(
 }
 
 // Free function: Computes the reprojection error between projected points and the original image points.
-double compute_reprojection_error(
+std::vector<double> compute_reprojection_error(
     const std::vector<Vector2D>& projected_points,
     const std::vector<Vector2D>& image_points
 ) {
     assert(projected_points.size() == image_points.size());
-    double total_error = 0.0;
+    double error = 0.0;
+    std::vector<double> error_vec;
     int n = projected_points.size();
     for (int i = 0; i < n; ++i) {
         double dx = projected_points[i].x() - image_points[i].x();
         double dy = projected_points[i].y() - image_points[i].y();
-        total_error += dx * dx + dy * dy;
+        error = std::sqrt(dx * dx + dy * dy);
+        error_vec.push_back(error);
     }
-    return std::sqrt(total_error / n);
+    return error_vec;
 }
 
 
@@ -109,16 +123,22 @@ bool Calibration::calibration(
     //--------------------------------------------------------------------------------------------------------------
     // implementation starts ...
     try {
+        // =====================================================================================================================
         // check if input is valid (e.g., number of correspondences >= 6, sizes of 2D/3D points must match)
+        // =====================================================================================================================
 
         int size_2d = points_2d.size();
         int size_3d = points_3d.size();
-        if (size_2d < 6) throw std::runtime_error("number of correspondences must >= 6, got " + std::to_string(size_2d));
-        if (size_3d < 6) throw std::runtime_error("number of correspondences must >= 6, got " + std::to_string(size_3d));
-        if (size_2d != size_3d) throw std::runtime_error("sizes of 2D/3D points must match, got " + std::to_string(size_2d) + " and " + std::to_string(size_3d));
+        if (size_2d < 6) throw std::invalid_argument("number of correspondences must >= 6, got " + std::to_string(size_2d));
+        if (size_3d < 6) throw std::invalid_argument("number of correspondences must >= 6, got " + std::to_string(size_3d));
+        if (size_2d != size_3d) throw std::invalid_argument("sizes of 2D/3D points must match, got " + std::to_string(size_2d) + " and " + std::to_string(size_3d));
 
+        // =====================================================================================================================
         // construct the P matrix (so P * m = 0).
-        Matrix P(2*size_3d, 12, 0.0);
+        // =====================================================================================================================
+
+        int row_num = 2*size_3d;
+        Matrix P(row_num, 12, 0.0);
         std::vector<double> tmp;
 
         for (int i = 0; i < size_3d; i++) {                             // For 3D-2D point correspondence 
@@ -139,28 +159,40 @@ bool Calibration::calibration(
         }
         std::cout << "P: \n" << P << std::endl;
 
+        // =====================================================================================================================
         // solve for M (the whole projection matrix, i.e., M = K * [R, t]) using SVD decomposition.
-        // Optional: you can check if your M is correct by applying M on the 3D points. If correct, the projected point
-        //           should be very close to your input images points.
+        // =====================================================================================================================
 
-        Matrix U(2*size_3d, 2*size_3d, 0.0);
-        Matrix S(2*size_3d, 12, 0.0);
+        Matrix U(row_num, row_num, 0.0);
+        Matrix S(row_num, 12, 0.0);
         Matrix V(12, 12, 0.0);
         svd_decompose(P, U, S, V);
 
+        // check if the SVD result is correct
+        if (equal_matrices(U * transpose(U), identity(row_num, 1))) throw std::invalid_argument("U * U^T must be identity!");
+        if (equal_matrices(V * transpose(V), identity(12, 1))) throw std::invalid_argument("V * V^T must be identity!");
+        if (equal_matrices(P, U * S * transpose(V))) throw std::invalid_argument("P != U * S * V^T!");
+        
+        Matrix m(12, 1, V.get_column(11).data());
         Matrix svd_M(3, 4, V.get_column(11).data());
         Matrix33 A(svd_M);
         Matrix b(3, 1, svd_M.get_column(3).data());
-        // std::cout << "The SVD-solved matrix M: \n" << svd_M << std::endl;
-        // std::cout << "The SVD-solved matrix A: \n" << A << std::endl;
-        // std::cout << "The SVD-solved matrix b: \n" << b << std::endl;
 
+        std::cout << "Is |Pm|^2 close to 0? |Pm|^2:\n" << (P*m).get_column(0).length2() << std::endl << std::endl;
+
+        // =====================================================================================================================
         // extract intrinsic parameters from M.
+        // =====================================================================================================================
 
         Vector3D a1xa3 = cross(A.get_row(0), A.get_row(2));
         Vector3D a2xa3 = cross(A.get_row(1), A.get_row(2));
 
-        double rho = 1 / A.get_row(2).length();
+        int rho_sign = 1;
+        if (b[2][0] < 0) {
+            rho_sign = -1;
+        }
+
+        double rho = rho_sign / A.get_row(2).length();
         cx = std::pow(rho, 2) * dot(A.get_row(0), A.get_row(2));
         cy = std::pow(rho, 2) * dot(A.get_row(1), A.get_row(2));
         double cos_theta = (-1) * dot(a1xa3, a2xa3) / (a1xa3.length() * a2xa3.length());
@@ -168,7 +200,9 @@ bool Calibration::calibration(
         fy = std::pow(rho, 2) * a2xa3.length();
         s = fx * cot_from_cos(cos_theta);
 
+        // =====================================================================================================================
         // extract extrinsic parameters from M.
+        // =====================================================================================================================
         
         Matrix K(3, 3, std::vector<double>({fx, s, cx, 0, fy, cy, 0, 0, 1}));
         Vector3D r1 = a2xa3.normalize();
@@ -184,16 +218,29 @@ bool Calibration::calibration(
         } else {
             throw std::runtime_error("expected a square matrix, got " + std::to_string(K.rows()) + " by " + std::to_string(K.cols()));
         }
+        std::cout << "The chosen rho: \n" << rho << std::endl <<  std::endl;
+        std::cout << "The solved matrix K: \n" << K << std::endl;
+        std::cout << "The solved matrix R: \n" << R << std::endl;
+        std::cout << "The solved matrix t: \n" << t << std::endl;
+        std::cout << "The focal length fx fy: \n" << fx << "\t" << fy << std::endl << std::endl;
         
-        std::vector<Vector2D> projected_2d = project_3D_to_2D(points_3d, K, R, t);
-        double err =  compute_reprojection_error(projected_2d, points_2d);
+        // =====================================================================================================================
+        // verify the error.
+        // =====================================================================================================================
 
-        std::cout << "the projected \t the ground truth: \n";
+        std::vector<Vector2D> projected_2d = project_3D_to_2D(points_3d, K, R, t);
+        auto err_vec = compute_reprojection_error(projected_2d, points_2d);
+
+        std::cout << "the projected\t|  ground truth\t|  error" << std::endl;
         for (size_t i = 0; i < projected_2d.size(); i++) {
-            std::cout << projected_2d[i] << "\t" << points_2d[i] << std::endl;
+            std::cout << projected_2d[i] << "\t|  " << points_2d[i] << "\t|  " << err_vec[i] << std::endl;
         }
-        std::cout << "the error (sum):\n" << err << std::endl;
-        
+        double err_sum = 0.0;
+        for (double num: err_vec) {
+            err_sum += num;
+        }
+        std::cout << "the reprojection error (avg): " << err_sum/err_vec.size() << std::endl;
+
         return true;
     }
     catch(const std::exception& e) {
